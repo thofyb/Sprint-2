1f118385ad936440fd77d4517e5ffbd485a2a0f9
thofyb <bogdyich@gmail.com>
2021-09-13 20:44:32 +0300

    solution fix

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -1,13 +1,16 @@
 class ProblemD {
     fun main() {
-        val t = readInt();
+        val t = readInt()
         for (runs in 1..t) {
-            val n = readInt();
-            val arr = readLongArray(n);
-            arr.sort();
-            val avgMoney = arr.sum() / n;
-            val price = arr.find { it >= avgMoney }!!;
-            println(arr.filter { it >= price }.size * price);
+            val n = readInt()
+            val arr = readLongArray(n)
+            arr.sortDescending()
+            var max = 0L
+            for (j in 0 until n) {
+                if (arr[j] + arr[j] * j > max)
+                    max = arr[j] + arr[j] * j
+            }
+            println(max)
         }
     }
 }
